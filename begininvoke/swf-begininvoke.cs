ca2940e6931e223b3dbe6d8674e66d9a0c751c3b
Jackson Harper <jackson@novell.com>
2004-09-14 17:52:56 +0000

    New demo of using Control::BeginInvoke, updates a labels value from a second thread
    svn path=/trunk/winforms/; revision=33894

---
+++ begininvoke/swf-begininvoke.cs
@@ -0,0 +1,59 @@
+//
+// swf-begininvoke.cs
+//
+// Author(s):
+//  Jackson Harper (jackson@ximian.com)
+//
+// Copyright (c) 2004 Novell, Inc (http://www.novell.com)
+//
+
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace System.Windows.Forms {
+
+	public class BeginInvokeDemo : Form {
+
+		private Label label;
+
+		public BeginInvokeDemo ()
+		{
+			label = new Label ();
+			label.Text = DateTime.Now.ToLongTimeString ();
+			Controls.Add (label);
+		}
+
+		private delegate void updater ();
+
+		private void UpdateLabel ()
+		{
+			while (true) {
+				lock (this) {
+					label.BeginInvoke (new updater (DateUpdater));
+				}
+				Thread.Sleep (500);
+			}
+		}
+
+		private void DateUpdater ()
+		{
+			label.Text = DateTime.Now.ToLongTimeString ();
+			label.Refresh ();
+		}
+
+		public static void Main ()
+		{
+			BeginInvokeDemo demo = new BeginInvokeDemo ();
+
+			ThreadStart thread_start = new ThreadStart (demo.UpdateLabel);	
+			Thread worker = new Thread (thread_start);
+			worker.IsBackground = true;
+			worker.Start();
+
+			Application.Run (demo);
+		}
+	}
+}
+
