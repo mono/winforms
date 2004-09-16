e32b9a9282e8d7d6d7d3a401b5dd796cc750c26d
Jackson Harper <jackson@novell.com>
2004-09-16 18:47:20 +0000

    Lame test for the idle handler
    svn path=/trunk/winforms/; revision=33977

---
+++ idle/swf-idle.cs
@@ -0,0 +1,42 @@
+//
+// swf-idle.cs
+//
+// Author(s):
+//  Jackson Harper (jackson@ximian.com)
+//
+// Copyright (c) 2004 Novell, Inc (http://www.novell.com)
+//
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace System.Windows.Forms {
+
+	public class IdleDemo : Form {
+
+		private Label label;
+
+		public IdleDemo ()
+		{
+			label = new Label ();
+			label.Dock = DockStyle.Fill;
+			label.TextAlign = ContentAlignment.MiddleCenter;
+			Controls.Add (label);
+		}
+
+                private void IdleHandler (object sender, EventArgs e)
+                {
+                        label.Text = "Last Idle: " + DateTime.Now.Ticks;
+                }
+
+		public static void Main ()
+		{
+			IdleDemo demo = new IdleDemo ();
+
+                        Application.Idle += new EventHandler (demo.IdleHandler);
+			Application.Run (demo);
+		}
+	}
+}
+
