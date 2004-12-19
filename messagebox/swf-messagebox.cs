bf90913fd0bb8bf44a09a5d87cae45d6a196c3d3
Peter Dennis Bartok <pbartok@mono-cvs.ximian.com>
2004-12-19 09:54:29 +0000

    - Initial check-in
    svn path=/trunk/winforms/; revision=37931

---
+++ messagebox/swf-messagebox.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MyFormProject
+{
+	public class MainForm : System.Windows.Forms.Form
+	{
+		static int	count;
+
+		// Calendar
+		private System.Windows.Forms.Button button1;
+
+		public MainForm()
+		{
+			InitializeComponent();
+			
+		}
+
+		private void InitializeComponent()
+		{
+			count = 0;
+
+			this.button1 = new Button();
+
+			this.button1.Location = new System.Drawing.Point(0, 0);
+			this.button1.Name = "label1";
+			this.button1.Size = new System.Drawing.Size(472, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Click me for OK MessageBox";
+			this.button1.Dock = DockStyle.Fill;
+			this.button1.TextAlign = ContentAlignment.MiddleCenter;
+
+			this.button1.Click +=new EventHandler(button1_Click);
+
+			// 
+			// MainForm
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(472, 382);
+
+			this.Controls.Add(this.button1);
+
+			this.Name = "MainForm";
+			this.Text = "SWF-MessageBox";
+		}
+
+		[STAThread]
+		static void Main() 
+		{
+			Application.Run(new MainForm());
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			DialogResult	result;
+
+			switch (count) {
+				case 0: {
+					result = MessageBox.Show("You must enter a name.", "Name Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+					this.button1.Text = "Last result was " + result + "\nClick to get OK/Cancel MessageBox";
+					break;
+				}
+
+				case 1: {
+					result = MessageBox.Show("You must enter a name.", "Name Entry Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+					this.button1.Text = "Last result was " + result + "\nClick me for Yes/No MessageBox";
+					break;
+				}
+
+				case 2: {
+					result = MessageBox.Show("You must enter a name.", "Name Entry Error", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+					this.button1.Text = "Last result was " + result + "\nClick to exit the test application";
+					break;
+				}
+
+				case 3: {
+					Application.Exit();
+					break;
+				}
+			}
+
+			count++;
+		}
+	}
+}
