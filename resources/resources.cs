8b19e3d94eaf8873c7e103e3f94cb02a8d194cc1
Peter Dennis Bartok <pbartok@mono-cvs.ximian.com>
2005-02-10 07:14:55 +0000

    - Test app for resources, not yet complete
    svn path=/trunk/winforms/; revision=40368

---
+++ resources/resources.cs
@@ -0,0 +1,12 @@
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ResourceTest {
+	class ResourceTest : Form {
+		static void Main () {
+			Application.Run();
+		}
+	}
+}
