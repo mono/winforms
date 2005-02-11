364db2b47acf02fe79a5a9d159e85d942937875b
Peter Dennis Bartok <pbartok@mono-cvs.ximian.com>
2005-02-11 23:20:39 +0000

    - Test application to verify SWF type converters
    svn path=/trunk/winforms/; revision=40506

---
+++ converters/converters.cs
@@ -0,0 +1,257 @@
+//
+// This is a bit overboard for a test app, but I'm hoping it 
+// can be gutted and used as a base for other test apps, trying
+// to set a good example :-)
+//
+
+
+using System;
+using System.Globalization;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MWFTestApplication {
+	class MainWindow : System.Windows.Forms.Form {	
+		static MainWindow	main_window;
+		static int		test_no		= 1;
+		static int		failed		= 0;
+		static int		debug		= 0;
+		static int		verbose		= 0;
+		static bool		visual		= false;
+		static bool		exception	= false;
+
+		static Control		active;
+
+		public void CheckConvert(int expect_from, int expect_to, TypeConverter conv, Type type) {
+			int from_count	 = 0;
+			int to_count	 = 0;
+
+			foreach (Type t in typeof(int).Assembly.GetTypes ()) {
+				if (conv.CanConvertFrom(null, t)) {
+					from_count++;
+					if (debug > 0) {
+						Console.WriteLine("{0}: Conversion from {1} supported", conv.ToString(), t.ToString());
+					}
+				}
+
+				if (conv.CanConvertTo(null, t)) {
+					to_count++;
+					if (debug > 0) {
+						Console.WriteLine("{0}: Conversion to {1} supported", conv.ToString(), t.ToString());
+					}
+				}
+			}
+
+			foreach (Type t in type.Assembly.GetTypes ()) {
+				if (conv.CanConvertFrom(null, t)) {
+					from_count++;
+					if (debug > 0) {
+						Console.WriteLine("{0}: Conversion from {1} supported", conv.ToString(), t.ToString());
+					}
+				}
+
+				if (conv.CanConvertTo(null, t)) {
+					to_count++;
+					if (debug > 0) {
+						Console.WriteLine("{0}: Conversion to {1} supported", conv.ToString(), t.ToString());
+					}
+				}
+			}
+
+			if (from_count != expect_from) {
+				if (verbose > 0) {
+					Console.WriteLine("{0}: ConvertFrom expected {1}, returned {2}", conv.ToString(), expect_from, from_count);
+				}
+				failed++;
+			}
+
+			if (to_count != expect_to) {
+				if (verbose > 0) {
+					Console.WriteLine("{0}: ConvertTo expected {1}, returned {2}", conv.ToString(), expect_to, to_count);
+				}
+				failed++;
+			}
+
+		}
+
+		public void CheckConversion(object check, object expect, TypeConverter conv, Type type) {
+			object obj;
+			object result;
+
+			if (debug > 0) {
+				Console.WriteLine("{0}: CheckConversion, checking {1}({2}) <-> {3}({4})", conv.ToString(), check.GetType().ToString(), check.ToString(), type.ToString(), expect.ToString());
+			}
+
+			obj = conv.ConvertTo(null, CultureInfo.InvariantCulture, check, type);
+
+			// Intermediate verification
+			if (!obj.Equals(expect)) {
+				failed++;
+				if (verbose > 0) {
+					Console.WriteLine("{0}: ConvertTo failed, type {1}, expected {2}, got {3}", conv.ToString(), type.ToString(), expect.ToString(), obj.ToString());
+				}
+			}
+
+			result = conv.ConvertFrom(null, CultureInfo.InvariantCulture, obj);
+
+			// Roundtrip check
+			if (!check.Equals(result)) {
+				failed++;
+				if (verbose > 0) {
+					Console.WriteLine("{0}: ConvertTo/ConvertFrom roundtrip failed, type {1}", conv.ToString(), type.ToString());
+				}
+			}
+		}
+
+		public void CheckStandardValues(object[] expected, TypeConverter conv) {
+			TypeConverter.StandardValuesCollection	values;
+
+			values = conv.GetStandardValues(null);
+
+			if (values == null && expected != null) {
+				failed++;
+				if (verbose > 0) {
+					Console.WriteLine("{0}: GetStandardValues expected {0} values, got null", conv.ToString(), expected.Length);
+				}
+				return;
+			}
+
+			if (values != null && expected == null) {
+				failed++;
+				if (verbose > 0) {
+					Console.WriteLine("{0}: GetStandardValues expected no values, got {1} values", conv.ToString(), values.Count);
+				}
+				return;
+			}
+
+			if (values != null) {
+				if (values.Count != expected.Length) {
+					if (verbose > 0) {
+						Console.WriteLine("{0} GetStandardValues expected {1} values, returned {2}", conv.ToString(), expected.Length, values.Count);
+					}
+					failed++;
+				}
+
+				if (debug > 0) {
+					Console.WriteLine("{0} Got {1} standard values", conv.ToString(), values.Count);
+				}
+
+				for (int i = 0; i < Math.Min(values.Count, expected.Length); i++) {
+					if (debug > 0) {
+						Console.WriteLine("{0}: Index {1:2} expecting:{2}, returned {3}", conv.ToString(), i, expected[i], values[i]);
+					}
+
+					if (expected[i].Equals(values[i])) {
+						if (verbose > 0) {
+							Console.WriteLine("{0}: GetStandardValues Index {1} values don't match ({2} != {3})", conv.ToString(), i, expected[i], values[i]);
+						}
+						failed++;
+
+					}
+				}
+			}
+		}
+
+		public void CheckStandardValuesExclusive(bool expect, TypeConverter conv) {
+			bool result;
+
+			result = conv.GetStandardValuesExclusive(null);
+
+			if (result != expect) {
+				failed++;
+				if (verbose > 0) {
+					Console.WriteLine("{0}: GetStandardValuesExclusive expected {1}, returned {2}", conv.ToString(), expect, result);
+				}
+			}
+		}
+
+		public void CheckStandardValuesSupported(bool expect, TypeConverter conv) {
+			bool result;
+
+			result = conv.GetStandardValuesSupported(null);
+
+			if (result != expect) {
+				failed++;
+				if (verbose > 0) {
+					Console.WriteLine("{0}: GetStandardValuesSupported expected {1}, returned {2}", conv.ToString(), expect, result);
+				}
+			}
+		}
+
+		public void TestKeysConverter() {
+			KeysConverter	key_conv;
+			string[]	expected = new string [] {
+						"Alt", "Back", "Control", "Delete", "End", "Enter", "F1", "F10", "F11", "F12", "F2",
+						"F3", "F4", "F5", "F6", "F7", "F8", "F9", "Home", "Insert", "Next", "Prior", "Shift" };
+
+			key_conv = new KeysConverter();
+
+			CheckConvert(1, 1, key_conv, typeof(Keys));
+			CheckConversion(Keys.Shift | Keys.F4, "Shift+F4", key_conv, typeof(string));
+			CheckStandardValues(expected, key_conv);
+			CheckStandardValuesSupported(true, key_conv);
+			CheckStandardValuesExclusive(false, key_conv);
+
+		}
+
+		public MainWindow() {
+			ClientSize = new System.Drawing.Size (520, 520);
+			Text = "SWF Converters Test App";
+
+			TestKeysConverter();
+
+			if (visual) {
+				if (failed == 0) {
+					MessageBox.Show("All Tests Passed!", "Success");
+				} else {
+					MessageBox.Show(failed + "tests failed, check the log", "Failure");
+				}
+			}
+		}		
+		
+		public static int Main(string[] args) {
+			if (args.Length > 0) {
+				for (int i=0; i< args.Length; i++) {
+					if ((args[i] == "-d") || (args[i] == "--debug")) {
+						debug++;
+						continue;
+					}
+
+					if ((args[i] == "-v") || (args[i] == "--verbose")) {
+						verbose++;
+						continue;
+					}
+
+					if ((args[i] == "-g") || (args[i] == "--gui")) {
+						visual = true;
+						continue;
+					}
+
+					if ((args[i] == "-e") || (args[i] == "--exception")) {
+						exception = true;
+						continue;
+					}
+
+					if ((args[i] == "-?") || (args[i] == "-h") || (args[i] == "--help")) {
+						Console.WriteLine("Usage: <cmd> [-d | --debug] [-v | --verbose] [-g | --gui] [e | --exception]");
+						return 0;
+					}
+				}
+			}
+			main_window = new MainWindow();
+			active = main_window;
+
+			// We don't want to run it, tests are already complete
+			if (visual) {
+				Application.Run(main_window);
+			}
+
+			if (failed == 0) {
+				return 0;
+			}
+
+			return failed;
+		}
+	}
+}
