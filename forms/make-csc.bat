2cf1b2fe8fd04ce21dc3a9e0dca350799a6d38a1
Peter Dennis Bartok <pbartok@mono-cvs.ximian.com>
2005-05-20 02:24:34 +0000

    - Switched msc switches to MS compatible format - Added make-csc.bat to easily allow building with csc
    svn path=/trunk/winforms/; revision=44783

---
+++ forms/make-csc.bat
@@ -0,0 +1,6 @@
+csc FormsTest.cs /resource:FormsTest.dotgnu.ico /resource:FormsTest.small_folder.ico /resource:FormsTest.small_text.ico /resource:FormsTest.test.ico /resource:"FormsTest.test 15bpp.bmp" /resource:"FormsTest.test 16bpp.bmp" /resource:"FormsTest.test 1bpp.bmp" /resource:"FormsTest.test 4bpp.bmp" /resource:"FormsTest.test 8bpp.bmp" /resource:FormsTest.test.bmp /r:System.Windows.Forms.dll /r:System.Drawing.dll
+csc FormsMdi.cs /r:System.Windows.Forms.dll /r:System.Drawing.dll
+csc FormsHello.cs /r:System.Windows.Forms.dll /r:System.Drawing.dll
+csc CustomControl.cs /r:System.Windows.Forms.dll /r:System.Drawing
+csc ImageViewer.cs /r:System.Windows.Forms.dll /r:System.Drawing.dll
+csc radiobuttons.cs /r:System.Windows.Forms.dll /r:System.Drawing.dll
