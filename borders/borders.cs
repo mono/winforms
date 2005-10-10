e24459f2e5a4a554086bacf692c22ab19fba004d
Peter Dennis Bartok <pbartok@mono-cvs.ximian.com>
2005-10-10 20:02:13 +0000

    - Added tests for form borders
    svn path=/trunk/winforms/; revision=51530

--- borders/borders.cs
+++ borders/borders.cs
@@ -11,10 +11,25 @@ namespace MyFormProject
 		private System.Windows.Forms.Panel panel2;
 		private System.Windows.Forms.Panel panel3;
 
+		private System.Windows.Forms.Form form1;
+		private System.Windows.Forms.Form form2;
+		private System.Windows.Forms.Form form3;
+		private System.Windows.Forms.Form form4;
+		private System.Windows.Forms.Form form5;
+		private System.Windows.Forms.Form form6;
+
 		// Labels within panels; describe options
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
+
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label13;
+
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label label16;
 		
 		public MainForm()
 		{
@@ -26,9 +41,24 @@ namespace MyFormProject
 			this.panel1 = new System.Windows.Forms.Panel();
 			this.panel2 = new System.Windows.Forms.Panel();
 			this.panel3 = new System.Windows.Forms.Panel();
+
+			this.form1 = new System.Windows.Forms.Form();
+			this.form2 = new System.Windows.Forms.Form();
+			this.form3 = new System.Windows.Forms.Form();
+			this.form4 = new System.Windows.Forms.Form();
+			this.form5 = new System.Windows.Forms.Form();
+			this.form6 = new System.Windows.Forms.Form();
+
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
+
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
 			// 
 			// panel1, label1
 			// 
@@ -77,6 +107,65 @@ namespace MyFormProject
 			this.label3.TabIndex = 6;
 			this.label3.Text = "BorderStyle=Fixed3D";
 			this.label3.TextAlign = ContentAlignment.MiddleCenter;
+
+
+			label11.Text = "FormBorderStyle.Fixed3D";
+			label11.Dock = DockStyle.Fill;
+			form1.FormBorderStyle = FormBorderStyle.Fixed3D;
+			form1.Controls.Add(this.label11);
+			form1.Show();
+
+			label12.Text = "FormBorderStyle.FixedDialog";
+			label12.Dock = DockStyle.Fill;
+			form2.FormBorderStyle = FormBorderStyle.FixedDialog;
+			form2.Controls.Add(this.label12);
+			form2.Show();
+
+			label13.Text = "FormBorderStyle.FixedSingle";
+			label13.Dock = DockStyle.Fill;
+			form3.FormBorderStyle = FormBorderStyle.FixedSingle;
+			form3.Controls.Add(this.label13);
+			form3.Show();
+
+			label14.Text = "FormBorderStyle.FixedToolWindow";
+			label14.Dock = DockStyle.Fill;
+			form4.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+			form4.Controls.Add(this.label14);
+			form4.Show();
+
+			label15.Text = "FormBorderStyle.Sizable";
+			label15.Dock = DockStyle.Fill;
+			form5.FormBorderStyle = FormBorderStyle.Sizable;
+			form5.Controls.Add(this.label15);
+			form5.Show();
+
+			label16.Text = "FormBorderStyle.SizableToolWindow";
+			label16.Dock = DockStyle.Fill;
+			form6.FormBorderStyle = FormBorderStyle.SizableToolWindow;
+			form6.Controls.Add(this.label16);
+			form6.Show();
+
+			// Position on screen
+			this.Location = new Point(10, 10);
+
+			form1.Location = new Point(10, 200);
+			form1.Size = new Size(200, 50);
+
+			form2.Location = new Point(10, 280);
+			form2.Size = new Size(200, 50);
+
+			form3.Location = new Point(10, 360);
+			form3.Size = new Size(200, 50);
+
+			form4.Location = new Point(10, 440);
+			form4.Size = new Size(200, 50);
+
+			form5.Location = new Point(10, 520);
+			form5.Size = new Size(200, 50);
+
+			form6.Location = new Point(10, 600);
+			form6.Size = new Size(200, 50);
+			
 
 			// 
 			// MainForm
