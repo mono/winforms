380b77aa11634f1e6d3c8d38a91f3940b5ff51c8
Andreia Gaita <avidigal@novell.com>
2007-04-13 17:06:52 +0000

    Demo for all types of Alignment and Buttons on a tabcontrol
    2007-04-13  Andreia Gaita <avidigal@novell.com>
    
    svn path=/trunk/winforms/; revision=75687

---
+++ tabcontrol/swf-tabcontrol2.cs
@@ -0,0 +1,75 @@
+//
+// swf-tabcontrol.cs
+//
+// Author(s):
+//  Jackson Harper (jackson@ximian.com)
+//
+// Copyright (C) Novell, Inc (http://www.novell.com)
+//
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace System.Windows.Forms {
+
+	public class TabControlDemo2 : Form {
+
+		public TabControlDemo2 () {
+
+			Controls.Add (MakeTab(0, 0, TabAlignment.Top, TabAppearance.Normal));
+			Controls.Add (MakeTab(250, 0, TabAlignment.Bottom, TabAppearance.Normal));
+			Controls.Add (MakeTab(500, 0, TabAlignment.Left, TabAppearance.Normal));
+			Controls.Add (MakeTab(750, 0, TabAlignment.Right, TabAppearance.Normal));
+
+			Controls.Add (MakeTab(0, 250, TabAlignment.Top, TabAppearance.FlatButtons));
+			Controls.Add (MakeTab(250, 250, TabAlignment.Bottom, TabAppearance.FlatButtons));
+			Controls.Add (MakeTab(500, 250, TabAlignment.Left, TabAppearance.FlatButtons));
+			Controls.Add (MakeTab(750, 250, TabAlignment.Right, TabAppearance.FlatButtons));
+			this.Size = new System.Drawing.Size(1000, 500);
+		}
+
+		private TabControl MakeTab(int x, int y, TabAlignment a, TabAppearance ap) {
+			TabControl tab = new TabControl();
+//			tab.Padding = new Point(20, 3);
+			tab.Location = new Point (x, y);
+			tab.Alignment = a;
+			tab.Appearance = ap;
+			tab.Controls.Add (CreateTabPage ("Red", Color.FromArgb (255, 255, 0, 0)));
+			tab.Controls.Add (CreateTabPage ("Orange", Color.FromArgb (255, 255, 153, 0)));
+			tab.Controls.Add (CreateTabPage ("Yellow", Color.FromArgb (255, 255, 255, 0)));
+			tab.Controls.Add (CreateTabPage ("Green", Color.FromArgb (255, 0, 153, 0)));
+			tab.Controls.Add (CreateTabPage ("Blue", Color.FromArgb (255, 0, 0, 255)));
+			tab.Controls.Add (CreateTabPage ("Purple", Color.FromArgb (255, 197, 0, 148)));
+
+			tab.Height = 200;
+			tab.Width = 200;
+			tab.SelectedIndex = 3;
+			return tab;
+		}
+
+		private TabPage CreateTabPage (string label, Color c)
+		{
+			TabPage res = new TabPage (label);
+			res.BackColor = c;
+			return res;
+		}
+
+		private void InitializeComponent() {
+			// 
+			// TabControlDemo
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.Size = new System.Drawing.Size(1000, 1000);
+			this.Name = "TabControlDemo";
+
+		}
+
+		public static void Main ()
+		{
+			Application.Run (new TabControlDemo2 ());
+		}
+	}
+}
+
